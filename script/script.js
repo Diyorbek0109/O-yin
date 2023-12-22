c2a515e00f5ff82006975fcf03694e68b95e76eb
Diyorbek0109 <diyorbohodr@gmail.com>
2023-12-22 16:46:48 +0300

    boshlanish

---
+++ script/script.js
@@ -0,0 +1,10 @@
+console.log("yes i working");
+let container = document.querySelector(".navbar");
+let nastroyka = document.getElementById("nastroyka");
+
+function parametr() {
+  //   alert("salom");
+  nastroyka.style.display = "flex";
+  container.style.filter = "blur(1px)";
+  nastroyka.style.filter = "none";
+}
